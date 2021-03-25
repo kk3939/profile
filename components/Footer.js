8c8d7af4f7bde63a35a6ab640a3e8cd578a64ab2
kk3939 <kyo51310@gmail.com>
2021-03-25 22:47:30 +0900

    create layout

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='flex items-center flex-wrap justify-center py-10 py-10'>
+            <p>Kyosuke Kubo </p>
+        </footer>
+    )
+}
+
+export default Footer
--- components/IndexMain.js
+++ components/IndexMain.js
@@ -1,7 +1,11 @@
 
 const IndexMain = () => {
     return (
-        ''
+        <div className='bg-gray-200 bg-opacity-25 flex-grow'>
+            <div className='container mx-auto'>
+            kakakaka
+            </div>
+        </div>
 
     )
 }
