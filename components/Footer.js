3da815c2520834cc0139d5021d5e0d3df9c1d043
kk3939 <kyo51310@gmail.com>
2021-03-29 10:04:31 +0900

    add sns on footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,69 +1,20 @@
-const Footer = ({showSns,setshowSns}) => {
-    return (
-        <footer className='flex  tablet:justify-between mobile:justify-center laptop:justify-start py-20 px-10 
-        items-center bg-gray-200 bg-opacity-25'>
-            
-            <p className='p-2'>
-                &copy; 2021 Kyosuke Kubo
-            </p>
-
-            <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={setshowSns}
-      >
-        SNS
-      </button>
-      {showSns ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={setshowSns}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={setshowSns}
-                  >
-                    Back
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-        </footer>
-    )
+const Footer = ({ showSns, setshowSns }) => {
+  return (
+    <footer className='flex justify-center py-10 px-10 items-center bg-gray-200 bg-opacity-25'>
+      <div className='justify-center'>
+        <div className='text-center py-2'>
+          <p className='block border-b-2 m-1 border-gray text-xl font-serif'>SNS</p>
+          <p className='font-serif py-1'>insta</p>
+          <p className='font-serif py-1'>twitter</p>
+          <p className='font-serif py-1'>facebook</p>
+          <p className='font-serif py-1'>qiita</p>
+          <p className='font-serif py-1'>zenn.</p>
+        
+        </div>
+        <p className='text-center font-serif text-base py-2'>&copy; 2021 Kyosuke Kubo</p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
