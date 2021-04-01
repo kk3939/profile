778ec83cfd329fa0f202d2ca989e3c61cc6953a9
kk3939 <kyo51310@gmail.com>
2021-04-01 21:48:34 +0900

    add comment

--- components/Tab.js
+++ components/Tab.js
@@ -17,11 +17,11 @@ const Tab = () => {
                             what I work on
                         </li>
                     </Link>
-                    <Link href='/publish'>
+                    {/* <Link href='/publish'>
                         <li className='block m-3 font-serif text-base border-b-2 border-white cursor-pointer hover:border-transparent'>
                             Publish
                         </li>
-                    </Link>
+                    </Link> */}
                 </ul>
             </nav>
         </div>
--- pages/publish.js
+++ pages/publish.js
@@ -1,27 +1,7 @@
 import React from 'react'
-import { useEffect } from 'react';
-// var client = require('cheerio-httpcli');
 
 const publish = () => {
 
-
-    // useEffect(async () => {
-    //     // Googleで「node.js」について検索する。
-    //     client.fetch('http://www.google.com/search', { q: 'node.js' }, function (err, $, res) {
-    //         // レスポンスヘッダを参照
-    //         console.log(res.headers);
-
-    //         // HTMLタイトルを表示
-    //         console.log($('title').text());
-
-    //         // リンク一覧を表示
-    //         $('a').each(function (idx) {
-    //             console.log($(this).attr('href'));
-    //         });
-    //     });
-    // }, [])
-
-    // console.log(articleData);
 
     return (
         <div><h1> publish </h1> </div>
